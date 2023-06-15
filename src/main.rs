d3f2426d43d6751e02c6d9b9c6584afa8bc83d93
Shriyans <shriyans@apolloagriculture.com>
2023-06-15 19:29:37 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use aws_sdk_s3 as s3;
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 use magick_rust::magick_wand_genesis;
 use std::net::SocketAddr;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 
 mod img_processing;
 use crate::img_processing::{image_router, state::ImgState};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt().with_target(false).pretty().init();
+
     let aws_configuration = aws_config::load_from_env().await;
     let s3_client = s3::Client::new(&aws_configuration);
 
@@ -27,11 +31,16 @@ async fn main() {
                 secret_salt: SECRET_SALT,
             }),
         )
-        .fallback(handler_404);
+        .fallback(handler_404)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
 
     let port = 6060;
     let addr: SocketAddr = format!("[::]:{}", port).parse().unwrap();
-    println!("Server running on {}\n", addr);
+    tracing::info!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(routes.into_make_service())
